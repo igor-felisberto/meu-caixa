83f4e38ce7c4d5ebc939eda2b26bfec85edbec78
igor-felisberto <iggoor2012@hotmail.com>
2026-09-19 06:08:38 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "meu-caixa-v1";
+
+const arquivos = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(arquivos);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resposta) => {
+      return resposta || fetch(event.request);
+    })
+  );
+});
